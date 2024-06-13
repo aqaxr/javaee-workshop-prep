7dc1e65599b38080e843243aa077cdc3c31b2d65
Patricia Stoffel <patricia.stoffel@dedalus.com>
2024-06-13 14:50:15 +0200

    added test

--- src/test/java/com/dedalus/resources/AnimalResourceIT.java
+++ src/test/java/com/dedalus/resources/AnimalResourceIT.java
@@ -1,5 +1,6 @@
-package com.dedalus;
+package com.dedalus.resources;
 
+import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
@@ -7,13 +8,14 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
+@TestHTTPEndpoint(AnimalResource.class)
 class AnimalResourceIT  {
     // Execute the same tests but in packaged mode.
 
     @Test
     void testGetAnimals(){
         given()
-                .when().get("/animal")
+                .when().get()
                 .then()
                 .statusCode(200)
                 .body("$.size()", is(5));
